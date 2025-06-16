e688ee3c54362f6f5ef7ae1b1dd586be736383f1
Workshop Script <750350+johnhwhite@users.noreply.github.com>
2025-06-16 09:16:06 +0000

    Create view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { ViewComponent } from './animal-profiles/view/view.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'animal-profiles/view/d3e440d8-8350-400e-b621-7639a5a2cd0a',
+  },
+  {
+    path: 'animal-profiles/view/:id',
+    component: ViewComponent,
+  },
+];
