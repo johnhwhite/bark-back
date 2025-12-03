247b494815ca97c067d772e41c11391fd8301be5
Workshop Script <750350+johnhwhite@users.noreply.github.com>
2025-12-03 20:04:54 +0000

    Data grid

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    pathMatch: 'full',
-    redirectTo: 'animal-profiles/view/a9bcfec1-c549-4957-909e-54f20ee0dda5',
+    loadComponent: () => import('./animal-profiles/list/list'),
   },
   {
     path: 'animal-profiles/view/:id',
